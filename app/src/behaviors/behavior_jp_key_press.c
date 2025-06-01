9b5ff8edeb4ef53744bf6547c02dd6d4cc70c767
kaito742 <96073283+kaito742@users.noreply.github.com>
2025-06-01 21:54:53 +0900

    modify behavior

--- app/src/behaviors/behavior_jp_key_press.c
+++ app/src/behaviors/behavior_jp_key_press.c
@@ -164,14 +164,14 @@ static int on_keymap_binding_pressed(struct zmk_behavior_binding * binding,
     // if(binding->param1 == JPUS) {
     //     jpmode = !jpmode;
     // }
-    // if (keycode == 0xF001) {
-    // }
 
     if (jpmode) {
         convert_jis_key(binding->param1, &needs_shift, &keycode, shift_already);
     }
 
-    keycode = (ZMK_HID_USAGE(HID_USAGE_KEY, HID_USAGE_KEY_KEYBOARD_LEFT_BRACKET_AND_LEFT_BRACE));
+    if (keycode == 0xF001) {
+        keycode = (ZMK_HID_USAGE(HID_USAGE_KEY, HID_USAGE_KEY_KEYBOARD_LEFT_BRACKET_AND_LEFT_BRACE));
+    }
     shift_encoded_key(needs_shift, shift_already);
 
     LOG_DBG("position %d keycode 0x%02X", event.position, keycode);
@@ -194,7 +194,9 @@ static int on_keymap_binding_released(struct zmk_behavior_binding * binding,
         convert_jis_key_release(binding->param1, &needs_shift, &keycode, shift_already);
     }
 
-    keycode = (ZMK_HID_USAGE(HID_USAGE_KEY, HID_USAGE_KEY_KEYBOARD_LEFT_BRACKET_AND_LEFT_BRACE));
+    if (keycode == 0xF001) {
+        keycode = (ZMK_HID_USAGE(HID_USAGE_KEY, HID_USAGE_KEY_KEYBOARD_LEFT_BRACKET_AND_LEFT_BRACE));
+    }
     int ret = raise_zmk_keycode_state_changed_from_encoded(keycode, false, event.timestamp);
 
     shift_encoded_key(needs_shift, shift_already);
